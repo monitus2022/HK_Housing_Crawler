1f6d8a4f75236b0b40ecde3d765da30ae75d20c5
monitus <monitus202201@proton.me>
2025-10-02 16:54:28 +0800

    added estate info processor

--- src/processors/agency.py
+++ src/processors/agency.py
@@ -2,14 +2,16 @@ import re
 from .base import BaseProcessor
 import sqlite3
 import pandas as pd
+import json
 
 class AgencyProcessor(BaseProcessor):
     def __init__(self):
         super().__init__()
         self.connect_db()
         self.transaction_file_path = "transactions.csv"
+        self.estate_info_json_path = "estate_info.json"
 
-    def process_transaction_table(self):
+    def process_transaction_table(self) -> pd.DataFrame:
         """
         Process the transaction table CSV file.
         Cleans and converts data types for specific columns.
@@ -21,16 +23,48 @@ class AgencyProcessor(BaseProcessor):
         df = df.drop(columns=["Change"], errors='ignore')
         df[int_columns] = df[int_columns].map(self.keep_only_numbers).astype("Int64")
         df[datetime_columns] = df[datetime_columns].map(self.convert_datetime)
+        df.to_csv(self.transaction_file_path, index=False)
         return df
     
-    def process_estate_info_json(self, json_data):
-        """
-        Process estate info from JSON data.
-        """
-        pass
+    def process_estate_info_json(self) -> list[dict]:
+        with open(self.estate_info_json_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        data = [self._process_single_estate_info(info) for info in data]
+        with open("processed_" + self.estate_info_json_path, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=2)
+        return data
+
+    def _process_single_estate_info(self, info) -> dict:
+        output_dict = {}
+        for key, value in info.items():
+            if key == "property_stat":
+                output_dict.update(value)
+            elif key == "market_stat":
+                if "yearly" in value:
+                    yearly_info = value["yearly"]
+                    yearly_info = {
+                        f"yearly_{k}": v 
+                        for k, v in yearly_info.items()
+                        }
+                    output_dict.update(yearly_info)
+                    value.pop("yearly")
+                output_dict.update({
+                    f"recent_{k}": v
+                    for k, v in value.items()
+                })
+            elif type(value) is dict:
+                output_dict.update({
+                    f"{key}_{sub_key}": sub_value
+                    for sub_key, sub_value in value.items()
+                })
+            elif "url" in key or key in ["icon", "hos", "show"]: 
+                continue
+            else:
+                output_dict[key] = value
+        return output_dict
 
     @staticmethod
-    def keep_only_numbers(cell):
+    def keep_only_numbers(cell) -> any:
         """
         Turns a cell with mixed characters into a cell with only numbers.
         """
@@ -41,7 +75,7 @@ class AgencyProcessor(BaseProcessor):
         return cell
 
     @staticmethod
-    def convert_datetime(cell):
+    def convert_datetime(cell) -> any:
         """
         Converts a cell to datetime format.
         """
---
+++ src/schema/estate_info.py
@@ -0,0 +1,57 @@
+estate_info_example = {
+    "id": "E000004419",
+    "name": "日出康城",
+    "region": {
+      "id": "20",
+      "name": "九龍"
+    },
+    "subregion": {
+      "id": "2010",
+      "name": "將軍澳"
+    },
+    "district": {
+      "id": "201005",
+      "name": "康城"
+    },
+    "sm_district": {
+      "id": "20100002",
+      "name": "康城"
+    },
+    "combined_district": {
+      "id": "201005-20100002",
+      "name": "康城"
+    },
+    "location": {
+      "lat": 22.2944,
+      "lon": 114.269558
+    },
+    "url_desc": "https://www.hkp.com.hk/zh-hk/estate/%E4%B9%9D%E9%BE%8D-%E5%BA%B7%E5%9F%8E-%E6%97%A5%E5%87%BA%E5%BA%B7%E5%9F%8E-E000004419",
+    "tx_history_url_desc": "https://www.hkp.com.hk/zh-hk/transaction-history/%E4%B9%9D%E9%BE%8D-%E6%97%A5%E5%87%BA%E5%BA%B7%E5%9F%8E-E000004419",
+    "buy_listing_url_desc": "https://www.hkp.com.hk/zh-hk/list/buy/%E6%97%A5%E5%87%BA%E5%BA%B7%E5%9F%8E-E-E000004419",
+    "rent_listing_url_desc": "https://www.hkp.com.hk/zh-hk/list/rent/%E6%97%A5%E5%87%BA%E5%BA%B7%E5%9F%8E-E-E000004419",
+    "photo": "https://wm.hkp.com.hk/img_wm_revamp.php?wm=hkp&src=http%3A%2F%2Fimg.midland.com.hk%2Fphoto_db%2Foutlook%2Festate%2FEE000004419_17.jpg",
+    "hos": "false",
+    "first_op_date": "2008-12-08T16:00:00.000Z",
+    "icon": "video",
+    "show": "true",
+    "property_stat": {
+      "sell_count": 1162,
+      "rent_count": 347
+    },
+    "market_stat": {
+      "ft_price": 0,
+      "net_ft_price": 11428,
+      "tx_count": 50,
+      "net_ft_price_chg": -2.63,
+      "pre_net_ft_price": 11737,
+      "ft_price_chg": 0,
+      "pre_ft_price": 0,
+      "total_tx_amount": 402274000,
+      "yearly": {
+        "tx_count": 601,
+        "total_tx_amount": 4574558680,
+        "net_ft_price": 11774.28,
+        "net_ft_price_chg": -0.46
+      }
+    }
+  }
